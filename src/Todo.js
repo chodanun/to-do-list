54a0b6decf8ba38418fd90970cba97af20d5744b
Chodanun Srinil <step_nilanam@hotmail.com>
2017-06-26 19:50:21 +0700

    concat to-do-list

--- src/Todo.js
+++ src/Todo.js
@@ -1,10 +1,52 @@
 import React, { Component } from 'react';
 
 export class Todo extends Component {
+	constructor(props) {
+	  super(props);
+	  this.state = {
+	  	inputText: '',
+	  	listItem: [],
+	  };
+	}
+
+	handleOnchangeText(e){
+		this.setState({
+			inputText: e.target.value ,
+		})
+	}
+	submitList(e){
+		this.setState({
+			listItem: [...this.state.listItem,this.state.inputText],
+		})
+	}
   render() {
     return (
         <div>
-          <h1>New Component</h1>
+          	<div className="col-lg-6">
+          		<div className="title">
+          			<h1>To-do-list</h1>
+          		</div>
+			    <div className="input-group">
+			      <input 
+			      	type="text" 
+			      	className="form-control" 
+			      	onChange = {this.handleOnchangeText.bind(this)}
+			      	value = {this.state.inputText}
+			      	placeholder="Search for..."/>
+			      <span className="input-group-btn">
+			        <button 
+			        	className="btn btn-default" 
+			        	onClick = {this.submitList.bind(this)}
+			        	type="button">Go!
+			        </button>
+			      </span>
+			    </div>
+			    {
+			    	this.state.listItem.map( (val, index) =>{
+			    		return <h3 key={index+val} >{val}</h3>
+			    	})
+			    }
+			  </div>
         </div>
     );
   }
